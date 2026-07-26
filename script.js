b6a969e49a23ace9eb04cdf2cd30ed1b37efa29f
eryayn01 <eryayn01@gmail.com>
2026-07-26 06:01:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
+// Bir Lokma Daha
+// script.js
 
+// Menü küçültme
+const header = document.querySelector("header");
+
+window.addEventListener("scroll", () => {
+
+    if (window.scrollY > 50) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+
+});
+
+
+// Sayfa animasyonları
+
+const observer = new IntersectionObserver((entries)=>{
+
+    entries.forEach(entry=>{
+
+        if(entry.isIntersecting){
+
+            entry.target.classList.add("show");
+
+        }
+
+    });
+
+},{
+    threshold:.15
+});
+
+document.querySelectorAll("section").forEach(section=>{
+
+    section.classList.add("hidden");
+
+    observer.observe(section);
+
+});
